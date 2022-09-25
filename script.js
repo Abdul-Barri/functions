27e57484832cb50f33d856ab15d392e3382c5542
Abdul-Barri <lawalabdulbarri@gmail.com>
2022-09-25 17:42:22 +0100

    functions in functions

--- script.js
+++ script.js
@@ -45,8 +45,60 @@ const checkIn = function (flightNum, passenger) {
 
 const newPassport = function (personObj) {
   personObj.passport = Math.trunc(Math.random() * 10000000000);
-}
+};
 
 newPassport(barraghan);
-checkIn(flight, barraghan)
+// checkIn(flight, barraghan);
 console.log(barraghan);
+
+///////////////////////////////
+
+const oneWord = function (str) {
+  return str.replace(/ /g, "").toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string ${fn(str)}`);
+  console.log(`Transformed by ${fn.name}`);
+};
+
+transformer("JavaScript is the best!", oneWord);
+transformer("JavaScript is the best!", upperFirstWord);
+
+////////////////////////////////////////////
+
+const myName = "Abdul Barri";
+const friendName = "Owologe";
+const thirdPersonName = "Habeeb";
+
+// const greet = function (greeting) {
+//   return function (name) {
+//     console.log(`${greeting} ${name}`);
+//   };
+// };
+
+const greet = (greeting) => {
+  return (name) => {
+    console.log(`${greeting} ${name}`);
+  };
+};
+
+const greeterHey = greet("Hey");
+
+const greeterHello = greet("Hello");
+
+const greeterAssalam = greet("Assalam Alaikum");
+
+greeterHey(myName);
+greeterAssalam(myName);
+greeterHello(friendName);
+// OR
+greet("Hey")(myName);
+greet("Assalam Alaikum")(myName);
+greet("Hello")(friendName);
